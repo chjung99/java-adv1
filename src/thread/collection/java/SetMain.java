a89dbf70c7181e1ca3f3254aa1f16d2d4b8228a7
chjung99 <cksgh1168@gmail.com>
2025-11-16 23:27:27 +0900

    feat: 섹션12. 동시성 컬렉션

---
+++ src/thread/collection/java/SetMain.java
@@ -0,0 +1,23 @@
+package thread.collection.java;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class SetMain {
+    public static void main(String[] args) {
+        Set<Integer> set = new CopyOnWriteArraySet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        System.out.println("set = " + set);
+
+        ConcurrentSkipListSet<Object> skipSet = new ConcurrentSkipListSet<>();
+        skipSet.add(3);
+        skipSet.add(1);
+        skipSet.add(2);
+        System.out.println("skipSet = " + skipSet);
+    }
+}
